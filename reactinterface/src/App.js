4e7a3b2be463d351d1a59e662f215f4bdf552e39
Mir ayon Elahi <mirayonelahi@Mirs-MBP.hitronhub.home>
2021-08-02 22:32:09 -0700

    add appontment form is created

--- reactinterface/src/App.js
+++ reactinterface/src/App.js
@@ -1,11 +1,13 @@
 import {BiArchive} from "react-icons/bi"
 import Search from "./components/Search";
+import AddAppoinment from "./components/AddAppoints";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl"> <BiArchive className="inline-block text-red-400 align-top" /> Your Appoinments</h1>
       <Search />
+      <AddAppoinment />
     </div>
   );
 }
