a5c085fc0a999f08f6a3fdc9ff68e427420eea2f
Mir ayon Elahi <mirayonelahi@Mirs-MBP.hitronhub.home>
2021-08-03 05:30:22 -0700

    search functionality added

--- reactinterface/src/App.js
+++ reactinterface/src/App.js
@@ -8,6 +8,18 @@ function App() {
 
 
   let [appoinmentList,setAppoinmentList] = useState([]);
+  let [query, setQuery] = useState("");
+
+  const filteredAppoinmentts = appoinmentList.filter(
+    item => {
+      return(
+        item.petName.toLowerCase().includes(query.toLocaleLowerCase())||
+        item.ownerName.toLowerCase().includes(query.toLocaleLowerCase())||
+        item.aptNotes.toLowerCase().includes(query.toLocaleLowerCase())
+
+      )
+    }
+  )
 
   //using fetch method to fetch the data
   const fetchData = useCallback(
@@ -31,12 +43,14 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3"> <BiCalendar className="inline-block text-red-400 align-top" /> Your Appoinments</h1>
       <AddAppoinment />
-      <Search />
+      <Search 
+      query={query}
+      onQueryChange= {myQuery => setQuery(myQuery) }
+      />
 
       <ul className="divide-y divide-gray-200">
-        {appoinmentList.map(
+        {filteredAppoinmentts.map(
           appoinment => (
-   
             <AppointmentInfo key={appoinment.id}
               appoinment={appoinment}
               onDeleteAppointment={appoinmentId=>{
