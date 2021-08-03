1f48138e31237c0ef2e550a433ef88da73f59c48
Mir ayon Elahi <mirayonelahi@Mirs-MBP.hitronhub.home>
2021-08-02 22:06:03 -0700

    added tailwindcss and add all the dependency to run this with cra

--- reactinterface/src/App.js
+++ reactinterface/src/App.js
@@ -2,8 +2,8 @@ import {BiArchive} from "react-icons/bi"
 
 function App() {
   return (
-    <div className="App">
-      <h1> <BiArchive /> Your Appoinmentss</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"> <BiArchive className="inline-block text-red-400 align-top" /> Your Appoinments</h1>
     </div>
   );
 }
