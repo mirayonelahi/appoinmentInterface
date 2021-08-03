0dcfb2459f8004e8f6cf514d937c1ce22dcec8f0
Mir ayon Elahi <mirayonelahi@Mirs-MBP.hitronhub.home>
2021-08-03 01:18:07 -0700

    useeffect and usecallback hooks and use fetch method

--- reactinterface/src/App.js
+++ reactinterface/src/App.js
@@ -1,11 +1,33 @@
+import { useState, useEffect , useCallback } from "react"
 import { BiCalendar } from "react-icons/bi"
 import Search from "./components/Search"
 import AddAppoinment from "./components/AddAppoints"
 import AppointmentInfo from "./components/appointmentInfo"
-import appoinmentList from "./data.json"
 
 function App() {
+
+
+  let [appoinmentList,setAppoinmentList] = useState([]);
+
+  //using fetch method to fetch the data
+  const fetchData = useCallback(
+    ()=>{
+      fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppoinmentList(data)
+      })
+
+    }, [])
+
+    useEffect(
+      () => {
+        fetchData()
+      },[fetchData]
+    )
+
   return (
+
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3"> <BiCalendar className="inline-block text-red-400 align-top" /> Your Appoinments</h1>
       <AddAppoinment />
